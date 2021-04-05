539417027e5caca80c0bb4cb54c610a2b736183b
Nastya <nastyadoma@mail.ru>
2021-04-05 14:59:52 +0300

    05/04

---
+++ src/main/java/model/Manufacturer.java
@@ -0,0 +1,20 @@
+package model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Manufacturer {
+    @Id
+    private Integer manufacturer_id;
+
+    private String name;
+    private String country;
+
+    @OneToMany(targetEntity = Model.class)
+    private List models;
+}
